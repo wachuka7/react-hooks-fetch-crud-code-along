720b3b968edfa0ad5d978564537d8b83a014c25d
wachuka7 <rachaelwachuka7@gmail.com>
2024-02-07 19:32:41 +0300

    Completed Lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,20 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:3000/items")
+      .then((r)=>r.json())
+      .then((items)=> setItems(items))
+  }, [])
+
+  function handleUpdateItem(updatedItem) {
+    console.log("In ShoppingCart:", updatedItem);
+  }
+
+  function handleAddItem(newItem){
+    setItems([...items, newItem])
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,14 +33,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+           <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} />
         ))}
       </ul>
     </div>
